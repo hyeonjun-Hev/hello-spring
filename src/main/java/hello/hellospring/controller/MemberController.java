06d1d9f20f0bfc4f84109545723551c66d1b3939
HamHyeonJun <hyunjun003@naver.com>
2023-02-02 13:24:44 +0900

    Controller 설명 (Spring Bean)

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,10 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
+//이 상태면 Spring 프로젝트가 실행 될떄
+//Spring container 에 Controller 어노테이션이 붙은 class 들은 객체가 자동 생성된다.
